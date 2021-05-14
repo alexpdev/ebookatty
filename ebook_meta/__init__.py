0de3940501e8015d27a5b17c75ff2f01d7f442c0
Alex <alexpdev@protonmail.com>
2021-05-13 20:13:06 -0700

    confirm

--- ebook_meta/__init__.py
+++ ebook_meta/__init__.py
@@ -1,9 +1,4 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
-import sys
-from ebook_meta.pybook_metadata import get_metadata, MetadataFetcher, cliparse
-
-def main():
-    cliparse(sys.argv[1:])
 
 __version__ = "0.1"
